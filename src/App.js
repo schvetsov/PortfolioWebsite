419027413ea226852504334a3f525222fb6c90d2
Stan Chvetsov <sac08xyz@gmail.com>
2019-01-04 00:03:32 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
+import logo from './logo.png';
+import node from './output3.png';
+import redux from './output4.png';
+import linkedin from './output5.png';
+import github from './output6.png';
 import './App.css';
 import About from './About';
 import MenuBar from './MenuBar';
@@ -10,18 +14,48 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      
       <div className="background">
+      <head>
+<title>Font Awesome 5 Icons</title>
+<meta name='viewport' content='width=device-width, initial-scale=1'/>
+<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'/>
+</head>
         <div className="title">
           <div className="title-container">
             <div className="title-sub-container">
               <span className="name">(Stan)islav Alexeevitch Chvetsov</span>
               <span className="name-bottom">--------------------------------------Aspiring software developer</span>
             </div>
+            {/* <div className="title-sub-container">
+              <span><img src={node} height = '36px'/></span>
+            </div> */}
+            <div className="title-sub-container">
+            <span><img src={logo} height = '36px'/></span>
+            </div>
+            <div className="title-sub-container">
+              <span><img src={node} height = '36px'/></span>
+            </div>
+            <div className="title-sub-container">
+            <span><img src={redux} height = '36px'/></span>
+            </div>
+            <div className="title-sub-container">
+            <span className="empty-space"></span>
+            </div>
+            <div className="title-sub-container" id="linkedInIcon">
+            <a href="https://www.linkedin.com/in/stan-chvetsov-224143a5/" target="_blank"><i id="linkedIn" className="fab fa-linkedin"></i></a>
+            </div>
+            <div className="title-sub-container">
+            {/* <span><img src={github} height = '36px'/></span> */}
+            <a href="https://github.com/schvetsov" target="_blank"><i id="gitHub" className="fab fa-github"></i></a>
+            </div>
           </div>
         </div>
-        <MenuBar />
+        <div className="module-border-wrap">
+          <MenuBar />
+        </div>
         <div className="footer">
-          - Stan Chvetsov 2019 -
+          - Page built with React -
         </div>
       </div>
       </BrowserRouter>
